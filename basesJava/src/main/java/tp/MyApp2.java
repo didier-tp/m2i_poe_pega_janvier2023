7c0558c8cd4f037a5f299a51e8704b39f588b4c8
DidierDefrance <didier@d-defrance.fr>
2023-01-18 16:15:47 +0100

    s

--- basesJava/src/main/java/tp/MyApp2.java
+++ basesJava/src/main/java/tp/MyApp2.java
@@ -6,8 +6,22 @@ public class MyApp2 {
 
 
 	public static void main(String[] args) {
-		testException();
-
+		//testException();
+		testException2();
+	}
+	
+	public static void testException2() {
+		Scanner reader = new Scanner(System.in);
+		System.out.println("x:");
+		double x = reader.nextDouble();
+		double z=0.0;
+		try {
+			z = racineCarree(x);
+		} catch (Exception e) {
+			//e.printStackTrace();
+			System.err.println("erreur:" + e.getMessage());
+		}
+		System.out.println("z=" + z);
 	}
 	
 	public static void testException() {
@@ -16,8 +30,19 @@ public class MyApp2 {
 		int x = reader.nextInt();
 		System.out.println("y:");
 		int y = reader.nextInt();
-		int z = x / y;
+		int z=0;
+		try {
+			z = x / y;
+		} catch (Exception e) {
+			//e.printStackTrace();
+			System.out.println("division par 0 interdite");
+		}
 		System.out.println("x=" + x +  " y=" + y +  " z=" + z);
+	}
+	
+	public static double racineCarree(double a) {
+		if(a<0) throw new RuntimeException("racine carre imposible sur a negatif");
+		return Math.sqrt(a);
 	}
 
 }
