26386c1cfd72cb3201525a70e2068dc72c510415
DidierDefrance <didier@d-defrance.fr>
2023-01-19 17:00:44 +0100

    s

--- basesJava2/src/main/java/tp/MyApp.java
+++ basesJava2/src/main/java/tp/MyApp.java
@@ -6,7 +6,7 @@ public class MyApp {
 
 	public static void main(String[] args) {
 		//lire un fichier produits.csv
-		//List de objets de la classe Produit(num,label,prix)
+		//List de objets de la classe Produit(numero,label,prix)
 		//calculer la moyenne (et éventuellement écart_type) des prix des produits
 		//générer un fichier stats.txt avec la moyenne calculée
 
