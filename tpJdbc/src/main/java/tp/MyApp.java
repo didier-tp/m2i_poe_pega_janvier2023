ee6ec98ba5be81bd85433ed03b86656f26798263
DidierDefrance <didier@d-defrance.fr>
2023-01-20 15:53:25 +0100

    s

--- tpJdbc/src/main/java/tp/MyApp.java
+++ tpJdbc/src/main/java/tp/MyApp.java
@@ -2,12 +2,15 @@ package tp;
 
 import tp.dao.DaoEleve;
 import tp.dao.DaoEleveJdbc;
+import tp.entity.Eleve;
 
 public class MyApp {
 
 	public static void main(String[] args) {
 		DaoEleve daoEleve = new DaoEleveJdbc();
-		daoEleve.findAllEleve();
+		for(Eleve e : daoEleve.findAllEleve()) {
+			System.out.println(e);
+		}
         //...
 	};
 
